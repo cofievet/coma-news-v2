4a1f965d51675a92fdc7a976ab030379c769bbdb
Mathieu Etcheverry <mathieu.etcheverry@cdbdx.biz>
2024-06-21 18:01:33 +0200

    feat: add articleCard [WIP]

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,5 +1,7 @@
 import { Head } from '@inertiajs/react'
+import { CircularProgress, Grid, Typography } from '@mui/material'
 import React from 'react'
+import ArticleCard from '../pages/article_card/article_card'
 
 export default function Home(props: any) {
   const { articles } = props
@@ -9,7 +11,17 @@ export default function Home(props: any) {
 
       <h1>Welcome to Inertia.js</h1>
 
-      <div className="flex gap-10 flex-col">
+      <Grid container spacing={2} padding={2}>
+        {articles.map((article: any) => (
+          <React.Fragment key={article.id}>
+            <Grid item xs={12}>
+              <ArticleCard article={article} />
+            </Grid>
+          </React.Fragment>
+        ))}
+      </Grid>
+
+      {/* <div className="flex gap-10 flex-col">
         {articles?.map((article: any) => (
           <React.Fragment key={article.id}>
             <div className="py-2">
@@ -20,7 +32,7 @@ export default function Home(props: any) {
             <hr />
           </React.Fragment>
         ))}
-      </div>
+      </div> */}
     </>
   )
 }
