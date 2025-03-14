29c81b3f96310a9b059addb44865a05da61c256f
Mathieu Etcheverry <mathieu.etcheverry@cdbdx.biz>
2025-03-14 15:11:41 +0100

    feat : Extract Cheerio in extract article service

--- app/controllers/article/create_articles_controller.ts
+++ app/controllers/article/create_articles_controller.ts
@@ -1,7 +1,6 @@
 import { createArticleValidator } from '#validators/create_article'
 import type { HttpContext } from '@adonisjs/core/http'
 import axios from 'axios'
-import * as cheerio from 'cheerio'
 import { ArticleRepository } from '#repositories/article_repository'
 import { inject } from '@adonisjs/core'
 import ExtractArticle from '#services/extract_article'
@@ -19,6 +18,7 @@ export default class CreateArticlesController {
   async execute({ request, response, auth }: HttpContext) {
     const data = await request.validateUsing(createArticleValidator)
     let $ = null
+    let resdata = null
 
     await axios
       .get(data.source, { headers: {
@@ -37,7 +37,7 @@ export default class CreateArticlesController {
     } })
       .then((res) => {
         if (res.status === 200) {
-          $ = cheerio.load(res.data)
+          resdata = res.data
         }
       })
       .catch((error) => {
@@ -45,11 +45,7 @@ export default class CreateArticlesController {
         throw new Error('Error while fetching the source URL')
       })
 
-    if (!$) {
-      throw new Error('Error while fetching the source URL')
-    }
-
-    const dataArticle = this.generateHtml(data.source, $)
+    const dataArticle = this.extractArticle.generateHtml(data.source, resdata)
     if (dataArticle === null) {
       throw new Error('The source URL is not supported')
     }
@@ -65,138 +61,5 @@ export default class CreateArticlesController {
       title,
     })
     return response.redirect().toRoute('article.render', { id: newArticle.id })
-  }
-
-  generateHtml(url: string, $: cheerio.CheerioAPI) {
-    
-    console.log("url: " + url)
-    
-    if (url.includes('eurosport.fr')) {
-      // Eurosport paragraphes, titles and blockquotes
-      const eurosportTags = [
-        '[data-testid=atom-body-paragraph]',
-        '[data-testid=atom-body-h2]',
-        '[data-testid=atom-body-blockquote] > div > blockquote',
-      ]
-      const eurosportAttrToRemove = ['data-testid', 'class']
-
-      const title = $('h1').text()
-      // Résumé de l'article
-      const resume = $('h2').text()
-      const author = $(
-        'div[data-testid=molecule-author-banner] > div > div:nth-child(1) > a'
-      ).text()
-
-      let content = this.extractArticle.GetContentRecursively($, $('div[data-testid=atom-body]'))
-
-
-      return {
-        title,
-        resume,
-        author,
-        content,
-      }
-    }
-
-    if (url.includes('lequipe.fr')) {
-      // Lequipe paragraphes, titles and blockquotes
-      const lequipeTags = [
-        '[class=Paragraph]',
-        '[class=TextFocus__subtitle]',
-        '[class=TextFocus__content]',
-        '[class=Article__embed]',
-      ]
-
-      const title = $('h1').text()
-      // Résumé de l'article
-      const resume = $('h2[class=Article__chapo]').text()
-      const author = "lequipe.fr"
-  
-      let content = this.extractArticle.GetContentRecursively($, $('div[class=article__body]'))  
-
-      return {
-        title,
-        resume,
-        author,
-        content,
-      }
-    }
-
-    
-    if (url.includes('lerugbynistere.fr')) {
-      // Lequipe paragraphes, titles and blockquotes
-      const lerugbynistereTags = [
-        '[id=article-body] p',
-        '[id=article-body] h2',
-      ]
-      const lerugbynistereAttrToRemove = [
-        'p[class=st-inarticle-paragraph]',
-      ]
-
-      const title = 'titre' // $('h1').text()
-      // Résumé de l'article
-      const resume = 'hello' //$('span[class=introduction]').text()
-      const author = "lerugbynistere.fr"
-
-      let content = this.GetContent($, [], [], 'div[class=article-content]')
-
-      return {
-        title,
-        resume,
-        author,
-        content,
-      }
-    }
-
-    
-    if (url.includes('fr.motorsport.com')) {
-      // Lequipe paragraphes, titles and blockquotes
-      const lerugbynistereAttrToRemove = [
-        'p[class=st-inarticle-paragraph]',
-      ]
-
-      const title = 'titre' // $('h1').text()
-      // Résumé de l'article
-      const resume = 'hello' //$('span[class=introduction]').text()
-      const author = "lerugbynistere.fr"
-
-      let content = this.GetContent($, [], [], 'div[class=ms-article__body]')
-      // let content = this.GetContentRecursively($, $('div[class=ms-article__body]'))
-
-      return {
-        title,
-        resume,
-        author,
-        content,
-      }
-    }
-
-    return null
-  }
-
-  private GetContent($: cheerio.CheerioAPI, tagsToAdd: string[], eurosportAttrToRemove: string[], parentTag: string) {
-    tagsToAdd.push('p')
-    let content = ''
-    const articleChildren = $(parentTag).children()
-    for (const articleChild of articleChildren) {
-      
-      for (const tag of tagsToAdd) {
-                
-        const foundElements = $(articleChild).find(tag)
-        for (const elementToAdd of foundElements) {          
-          if (elementToAdd && $(elementToAdd).prop('outerHTML') !== '') {
-            for (const attr of eurosportAttrToRemove) {
-              $(elementToAdd).removeAttr(attr)
-              for (const child of $(elementToAdd).children()) {
-                $(child).removeAttr(attr)
-              }
-            }
-            content = content.concat($(elementToAdd).prop('outerHTML')!)
-          }
-        }
-      }
-
-    }
-    return content
   }
 }
--- app/services/extract_article.ts
+++ app/services/extract_article.ts
@@ -1,14 +1,127 @@
 import * as cheerio from 'cheerio'
 
 export default class ExtractArticle {
+
     
-    public GetContentRecursively($: cheerio.CheerioAPI, parentElement: any) {
+  generateHtml(url: string, resdata: any) {
+    
+    const $ = cheerio.load(resdata)
+
+    if (!$) {
+      throw new Error('Error while fetching the source URL')
+    }
+
+    console.log("url: " + url)
+    
+    if (url.includes('eurosport.fr')) {
+      // Eurosport paragraphes, titles and blockquotes
+      const eurosportTags = [
+        '[data-testid=atom-body-paragraph]',
+        '[data-testid=atom-body-h2]',
+        '[data-testid=atom-body-blockquote] > div > blockquote',
+      ]
+      const eurosportAttrToRemove = ['data-testid', 'class']
+
+      const title = $('h1').text()
+      // Résumé de l'article
+      const resume = $('h2').text()
+      const author = $(
+        'div[data-testid=molecule-author-banner] > div > div:nth-child(1) > a'
+      ).text()
+
+      let content = this.GetContentRecursively($, $('div[data-testid=atom-body]'))
+
+
+      return {
+        title,
+        resume,
+        author,
+        content,
+      }
+    }
+
+    if (url.includes('lequipe.fr')) {
+      // Lequipe paragraphes, titles and blockquotes
+      const lequipeTags = [
+        '[class=Paragraph]',
+        '[class=TextFocus__subtitle]',
+        '[class=TextFocus__content]',
+        '[class=Article__embed]',
+      ]
+
+      const title = $('h1').text()
+      // Résumé de l'article
+      const resume = $('h2[class=Article__chapo]').text()
+      const author = "lequipe.fr"
+  
+      let content = this.GetContentRecursively($, $('div[class=article__body]'))  
+
+      return {
+        title,
+        resume,
+        author,
+        content,
+      }
+    }
+
+    
+    if (url.includes('lerugbynistere.fr')) {
+      // Lequipe paragraphes, titles and blockquotes
+      const lerugbynistereTags = [
+        '[id=article-body] p',
+        '[id=article-body] h2',
+      ]
+      const lerugbynistereAttrToRemove = [
+        'p[class=st-inarticle-paragraph]',
+      ]
+
+      const title = 'titre' // $('h1').text()
+      // Résumé de l'article
+      const resume = 'hello' //$('span[class=introduction]').text()
+      const author = "lerugbynistere.fr"
+
+      let content = this.GetContentRecursively($, 'div[class=article-content]')
+
+      return {
+        title,
+        resume,
+        author,
+        content,
+      }
+    }
+
+    
+    if (url.includes('fr.motorsport.com')) {
+      // Lequipe paragraphes, titles and blockquotes
+      const lerugbynistereAttrToRemove = [
+        'p[class=st-inarticle-paragraph]',
+      ]
+
+      const title = 'titre' // $('h1').text()
+      // Résumé de l'article
+      const resume = 'hello' //$('span[class=introduction]').text()
+      const author = "lerugbynistere.fr"
+
+      let content = this.GetContentRecursively($, 'div[class=ms-article__body]')
+
+      return {
+        title,
+        resume,
+        author,
+        content,
+      }
+    }
+
+    return null
+  }
+
+    private GetContentRecursively($: cheerio.CheerioAPI, parentElement: any) {
       const articleChildren = parentElement.children()
       for (const articleChild of articleChildren) {
         this.GetContentRecursively($, $(articleChild)) 
       }
-      if ($(parentElement).prop('outerHTML') !== '') {           
-        return $(parentElement).prop('outerHTML')
+      if ($(parentElement).prop('outerHTML') !== '' && $(parentElement).prop('outerHTML') !== null) {           
+        return $(parentElement).prop('outerHTML') ?? ''
       }
       return ''
     }
